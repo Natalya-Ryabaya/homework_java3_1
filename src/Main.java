c12c78119cac0ebd59684ab051ffa3bb54ce0780
Natalya Ryabaya <natalir.80@gmail.com>
2020-10-16 21:24:37 +0300

    создание репозитория

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = (int)656.60F;
+        int mile = 20;
+        int bonus = ticket/mile;
+        System.out.println(bonus);
+    }
+}
